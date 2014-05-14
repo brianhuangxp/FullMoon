de7512dc6875d33a3c3e2f7b94968079e3dd68f8
x-iron <yecongde@gmail.com>
2014-05-14 13:52:28 +0800

    add nodejs debug mode - api mock

---
+++ fm-assembly/src/main/webapp/browser/default/_resources/js/apiMock.js
@@ -0,0 +1,44 @@
+define(['angular', 'config'], function (angular, config) {
+    'use strict';
+    angular.module('myApp.api', [])
+        .provider('userApi', function () {
+            this.$get = ['$cookies', '$http', '$q', '$timeout', 'loader', function ($cookies, $http, $q, $timeout, loader) {
+                return {
+                    login: function (name, pass) {
+                        loader.show();
+                        var deferred = $q.defer();
+                        $timeout(function () {
+                            $cookies.sessid = '' + Math.random();
+                            loader.hide();
+                            deferred.resolve();
+                        }, 1000);
+                        return deferred.promise;
+                    },
+                    checkLogin: function () {
+                        loader.show();
+                        var deferred = $q.defer();
+                        $timeout(function () {
+                            loader.hide();
+                            deferred.resolve(
+                                {
+                                    logined: $cookies.sessid != null,
+                                    name: $cookies.user
+                                }
+                            );
+                        }, 1000);
+                        return deferred.promise;
+                    },
+                    logout: function () {
+                        loader.show();
+                        var deferred = $q.defer();
+                        $timeout(function () {
+                            loader.hide();
+                            $cookies.sessid = undefined;
+                            deferred.resolve();
+                        }, 1000);
+                        return deferred.promise;
+                    }
+                };
+            }];
+        });
+});
--- fm-assembly/src/main/webapp/browser/default/_resources/js/main.js
+++ fm-assembly/src/main/webapp/browser/default/_resources/js/main.js
@@ -1,24 +1,28 @@
-require.config({
-    baseUrl: 'components',
-    paths: {
-        text: '../resources/js/lib/requireJs/text',
-        app: '../resources/js/app',
-        angular: '../resources/js/lib/angular/angular',
-        'angular-route': '../resources/js/lib/angular/angular-route.min',
-        'angular-cookies': '../resources/js/lib/angular/angular-cookies.min',
-        underscore: '../resources/js/lib/underscore/underscore-min',
-        common: '../resources/js/common',
-        api: '../resources/js/api',
-        config: '../resources/js/config'
-    },
-    shim: {
-        'angular': {'exports': 'angular'},
-        'underscore': {'exports': '_'},
-        'angular-route': { deps: ['angular']},
-        'angular-cookies': { deps: ['angular']}
-    },
-    urlArgs: 'v=0.1'
-});
+(function() {
+    var nodeJsDebug = false;
+    require.config({
+        baseUrl: 'components',
+        paths: {
+            text: '../resources/js/lib/requireJs/text',
+            app: '../resources/js/app',
+            angular: '../resources/js/lib/angular/angular',
+            'angular-route': '../resources/js/lib/angular/angular-route.min',
+            'angular-cookies': '../resources/js/lib/angular/angular-cookies.min',
+            underscore: '../resources/js/lib/underscore/underscore-min',
+            common: '../resources/js/common',
+            api: '../resources/js/api' + (nodeJsDebug ? 'Mock' : ''),
+            config: '../resources/js/config'
+        },
+        shim: {
+            'angular': {'exports': 'angular'},
+            'underscore': {'exports': '_'},
+            'angular-route': { deps: ['angular']},
+            'angular-cookies': { deps: ['angular']}
+        },
+        urlArgs: 'v=0.1'
+    });
+})();
+
 require(['angular','config', 'app'], function (angular, config) {
     angular.element(document).ready(
         function () {
